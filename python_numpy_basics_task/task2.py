2595d98ff9e0f9f0411e79f3f38dd5e4341ac9c9
Alyona1619 <108504553+Alyona1619@users.noreply.github.com>
2023-10-07 21:27:46 +0800

    Update task2.py

--- python_numpy_basics_task/task2.py
+++ python_numpy_basics_task/task2.py
@@ -5,7 +5,5 @@ img2 = np.loadtxt('files/img2.txt', skiprows=1)
 
 y1, x1 = np.where(img1 != 0)
 y2, x2 = np.where(img2 != 0)
-# print(min(y1), min(x1))
-# print(min(y2), min(x2))
 
 print(f"Offset (y:{min(y2) - min(y1)}, x:{min(x2) - min(x1)})")  # Offset (y:-4, x:10)
