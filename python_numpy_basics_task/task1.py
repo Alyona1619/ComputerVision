b45f663d208e24285be1d2092454e62a15d024d2
Alyona1619 <108504553+Alyona1619@users.noreply.github.com>
2023-10-07 19:54:56 +0800

    Update task1.py

--- python_numpy_basics_task/task1.py
+++ python_numpy_basics_task/task1.py
@@ -1,1 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
+for i in range(1, 7):
+    file_name = f"figure{i}.txt"
+
+    with open(file_name, 'r') as file:
+        min_size = float(file.readline())
+        _ = file.readline()
+
+        lines = np.loadtxt(file)
+
+        figure = [sum(line) for line in lines if 1 in line]
+
+        if not figure:
+            print(f"File {file_name} is empty")
+        else:
+            print(f"Nominal resolution {file_name}: {min_size / max(figure)}")
+
+
+# image = np.loadtxt('figure1.txt', skiprows=1)
+# plt.imshow(image, cmap='gray')
+# plt.show()
